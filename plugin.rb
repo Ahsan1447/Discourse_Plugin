a2b5a28d1d6549bc30866ab1dc3462ff69115b6d
Ahsan1447 <ahsanchaudary1447@gmail.com>
2025-06-25 13:42:05 +0500

    Remove Extra code

--- plugin.rb
+++ plugin.rb
@@ -9,12 +9,10 @@ gem "sentry-rails"
 
 enabled_site_setting :salla_serializers_enabled
 
-gem "sentry-ruby"
-gem "sentry-rails"
+require 'sentry-ruby'
+require 'sentry-rails'
 
 after_initialize do
-  require 'sentry-ruby'
-  require 'sentry-rails'
 
   %w[
     basic_category_serializer_extension
@@ -26,16 +24,16 @@ after_initialize do
   end
 
   if ENV['SENTRY_DSN'].present?
-    Sentry.init do |config|
+    ::Sentry.init do |config|
       config.dsn = ENV['SENTRY_DSN']
       config.environment = ENV['SENTRY_ENV'] || 'staging'
-      
+
       # get breadcrumbs from logs
       config.breadcrumbs_logger = [:active_support_logger, :http_logger]
       # Add data like request headers and IP for users, if applicable;
       # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
       config.send_default_pii = true
-      
+
       config.traces_sample_rate = 0.1
       config.sample_rate = 0.1
 
